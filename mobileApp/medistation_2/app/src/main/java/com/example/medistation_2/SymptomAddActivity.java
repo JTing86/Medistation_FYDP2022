0d28e9c927a879e10432b4327068de9234bbac90
David <r344zhan@edu.uwaterloo.ca>
2021-06-15 20:12:26 -0400

    Got Database Working

--- mobileApp/medistation_2/app/src/main/java/com/example/medistation_2/SymptomAddActivity.java
+++ mobileApp/medistation_2/app/src/main/java/com/example/medistation_2/SymptomAddActivity.java
@@ -1,5 +1,6 @@
 package com.example.medistation_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,9 +9,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class SymptomAddActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymptomAddActivity extends AppCompatActivity {
+    private static final String TAG = SymptomAddActivity.class.getSimpleName();
     private Button SymptomAddButton;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +40,28 @@ public class SymptomAddActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
+
                 EditText symptom = (EditText)findViewById(R.id.SymptomInput);
-                Log.d("Test", symptom.getText().toString());
+                myRef.setValue(symptom.getText().toString());
+                Log.d("Success",symptom.getText().toString());
+
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "Value is: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
             }
         });
-    }
-    public void AddSymptoms (View view)
-    {
 
     }
 }
