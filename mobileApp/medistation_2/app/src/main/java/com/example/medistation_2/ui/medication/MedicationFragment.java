a7b8ebb89018acff45d2d2e4f804e19fed119bbf
David <r344zhan@edu.uwaterloo.ca>
2021-10-21 00:47:40 -0400

    medication schedule work in progress

--- mobileApp/medistation_2/app/src/main/java/com/example/medistation_2/ui/medication/MedicationFragment.java
+++ mobileApp/medistation_2/app/src/main/java/com/example/medistation_2/ui/medication/MedicationFragment.java
@@ -1,5 +1,6 @@
 package com.example.medistation_2.ui.medication;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -8,15 +9,26 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.medistation_2.R;
+import com.example.medistation_2.ui.profile.ProfileFragment;
 
 public class MedicationFragment extends Fragment {
 
     private MedicationViewModel mViewModel;
+    private static final String TAG = MedicationFragment.class.getSimpleName();
 
     public static MedicationFragment newInstance() {
         return new MedicationFragment();
@@ -35,4 +47,24 @@ public class MedicationFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+        ToggleButton testButton = (ToggleButton) view.findViewById(R.id.toggleButton);
+        Button buttonpress = view.findViewById(R.id.button);
+        buttonpress.setOnClickListener(v -> {
+            Log.d(TAG,"Buttonnn Pressed");
+            Boolean ToggleButtonState = testButton.isChecked();
+            Log.d(TAG,String.valueOf(ToggleButtonState));
+            TableLayout table = (TableLayout)view.findViewById(R.id.medi);
+            for(int i=0;i<3;++i) {
+                    // Inflate your row "template" and fill out the fields.
+                    TableRow row = (TableRow)LayoutInflater.from(requireActivity()).inflate(R.layout.schedule_table_format, null);
+                    ((TextView)row.findViewById(R.id.attrib_name)).setText("name");
+                    //((TextView)row.findViewById(R.id.attrib_value)).setText("value");
+                    table.addView(row);
+                table.requestLayout();     // Not sure if this is needed.
+            }
+        });
+    }
 }
